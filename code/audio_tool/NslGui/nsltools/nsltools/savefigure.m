9a19b48b096d8b7e9128309fa0240d180284c85f
zhongpingDong12 <98571846+zhongpingDong12@users.noreply.github.com>
2022-08-05 12:15:04 +0800

    Delete code directory

--- code/audio_tool/NslGui/nsltools/nsltools/savefigure.m
+++
@@ -1,8 +0,0 @@
-function savefigure(figNum,figStr)
-% SAVEFIGURE(figNum,figNamev)
-%
-% savefigure saves figure number figNum, using the name figName
-% (actually saving 2 files, figName.m and figName.mat). 
-%
-print('-dmfile',['-f',num2str(figNum)],figStr)
-
--- code/eval_tools/word_pesq.m
+++
@@ -1,3 +0,0 @@
-ori_path  = 'C:\Users\Andrew\sally_file\eval_tools\eva_sally\test_all_sample\male_gf\waveform\oriTraining\';
-pre_path = 'C:\Users\Andrew\sally_file\eval_tools\eva_sally\test_all_sample\male_gf\waveform\training\';
-save_path = 'C:\Users\Andrew\sally_file\eval_tools\eva_sally\test_all_sample\male_gf\waveform\eva_training.mat';
